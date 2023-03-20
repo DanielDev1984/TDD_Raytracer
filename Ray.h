9481e66f83802d778a4434f342a061ba4ac90cbb
DanielDev1984 <60503176+DanielDev1984@users.noreply.github.com>
2023-03-20 09:34:54 +0100

    add new class for representing Rays (1st draft)

---
+++ Ray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ArithmeticStructures.h"
+class Ray
+{
+public:
+	Ray(ArithmeticStructures::HomogenousCoordinates rayOrigin, ArithmeticStructures::HomogenousCoordinates rayDirection) {
+		const auto& [orig_x, orig_y, orig_z, orig_w] = rayOrigin;
+		const auto& [dir_x, dir_y, dir_z, dir_w] = rayDirection;
+		m_as.setPoint(orig_x, orig_y, orig_z);
+		m_as.setVector(dir_x, dir_y, dir_z);
+	};
+
+
+	ArithmeticStructures::HomogenousCoordinates getOrigin() { return m_as.getPoint(); };
+	ArithmeticStructures::HomogenousCoordinates getDirection() { return m_as.getVector(); };
+private:
+	ArithmeticStructures m_as; //todo: not sure whether this really is a good implementation...
+};
+
