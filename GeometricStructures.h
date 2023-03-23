79b4e7a9cd4d5e462a21dc46a84f8bfd0991b2c1
DanielDev1984 <60503176+DanielDev1984@users.noreply.github.com>
2023-03-23 10:34:16 +0100

    implement standard algorithm for calculating sphere / line intersection

--- GeometricStructures.h
+++ GeometricStructures.h
@@ -14,10 +14,28 @@ public:
 		int getRadius() { return m_radius; };
 		ArithmeticStructures::HomogenousCoordinates getOrigin() { return m_origin; };
 		Intersections getIntersections(Ray ray) { 
+			// !! this intersection algorithm treats the ray as a line and will also return an intersection when the intersection lies "behind" the ray origin
+
 			// vector pointing from the center of the sphere to the origin of the ray
-			//todo: subtraction not implemented in arithmeticstructures
+			const auto vec_sphereToRay{ArithmeticStructures::subtractCoordinates(ray.getOrigin(), this->getOrigin())};
+			const auto a{ ArithmeticStructures::dotProduct(ray.getDirection(), ray.getDirection()) };
+			const auto b{ 2 * (ArithmeticStructures::dotProduct(ray.getDirection(), vec_sphereToRay)) };
+			const auto c{ ArithmeticStructures::dotProduct(vec_sphereToRay, vec_sphereToRay) -1.0 };
 			
-			
+			// standard way of determining LINE (!= ray) sphere intersection
+			const auto discriminant{pow(b,2) - 4*a*c};
+
+			// no intersections between ray and spehere if discriminant < 0
+			m_intersections.clear();
+			if (discriminant >= 0)
+			{
+				// always calculate 2 intersections even if the sphere is only hit tangentially or the origin of the ray lies within the sphere
+				const auto t1{(-1.0*b - sqrt(discriminant)) / (2*a) };
+				const auto t2{ (-1.0 * b + sqrt(discriminant)) / (2 * a) };
+				m_intersections.push_back(t1);
+				m_intersections.push_back(t2);
+			}
+
 			return m_intersections; 
 		
 		};
