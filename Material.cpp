8cb758d730bf5db6d0e4b69432981a57d4171e8e
DanielDev1984 <60503176+DanielDev1984@users.noreply.github.com>
2023-04-21 08:52:47 +0200

    add Material class

--- Material.cpp
+++ Material.cpp
@@ -1,1 +1,7 @@
 #include "Material.h"
+
+Material::Material(float ambientFactor, float diffuseFactor, float specularFactor, float shininessFactor):
+	m_ambientReflectionFactor{ ambientFactor }, 
+	m_diffuseReflectionFactor{ diffuseFactor }, 
+	m_specularReflectionFactor{ specularFactor }, 
+	m_shininessFactor{ shininessFactor } {};
--- Material.h
+++ Material.h
@@ -1,5 +1,16 @@
 #pragma once
 class Material
 {
+public:
+	Material(float ambientFactor, float diffuseFactor, float specularFactor, float shininessFactor);
+	float getAmbientReflectionFactor() { return m_ambientReflectionFactor; };
+	float getDiffuseReflectionFactor() { return m_diffuseReflectionFactor; };
+	float getSpecularReflectionFactor() { return m_specularReflectionFactor; };
+	float getShininessFactor() { return m_shininessFactor; };
+private:
+	float m_ambientReflectionFactor{};
+	float m_diffuseReflectionFactor{};
+	float m_specularReflectionFactor{};
+	float m_shininessFactor{};
 };
 
